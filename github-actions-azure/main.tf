d10c2e1c76cb7d6792b2414ca3be865f62232f71
Osvaldo <cantusantos@gmail.com>
2023-03-10 23:17:17 -0600

    new commit

--- github-actions-azure/main.tf
+++ github-actions-azure/main.tf
@@ -3,6 +3,7 @@ provider "azurerm" {
   use_oidc = true
 }
 
+# Creating the ResourceGroup "temp-auto-RG" , the name its get from variables.tf file
 resource "azurerm_resource_group" "oidc" {
   name     = var.resource_group_name
   location = var.location
