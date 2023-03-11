d1922b15c39effda7ddaf076e516bc138e909ac6
Osvaldo <cantusantos@gmail.com>
2023-03-10 21:06:05 -0600

    added pipeline workflow

--- github-actions-azure/variables.tf
+++ github-actions-azure/variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
   type        = string
   description = "(Optional) Name of resource group to create. Defaults to oidc-test."
-  default     = "luis-simple-09162022"
+  default     = "temp-auto-RG"
 }
 
 variable "location" {
   type        = string
   description = "(Optional) Azure region to use. Defaults to East US."
-  default     = "eastus"
+  default     = "East US"
 }
